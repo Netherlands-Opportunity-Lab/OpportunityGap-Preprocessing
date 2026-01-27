8daad9caceb5094068adbb6088a816eb07f46f4e
Sonya <jileaeva@ese.eur.nl>
2026-01-27 15:27:18 +0100

    Update script to S2.0 version

--- S2.0_create_cohort_data.R
+++ S2.0_create_cohort_data.R
@@ -5,12 +5,19 @@
 # (c) ODISSEI Social Data Science team 2025
 
 
+
+# clean workspace
+rm(list=ls())
+setwd("H:/IGM project/kansenkaart_preprocessing")
+options(scipen=999)
+excel_path <- 'H:/IGM project/Excel/output/'
+
+
+
 # input the desired config file here:
-# yml: age35, age21, age16, prim8, newborns
+# yml: age35, age21, age16, prim8_a, prim8_b, newborns_a, newborns_b
 cohort <- "age35"
 cfg_file <- paste0("config/", cohort, ".yml")
-
-excel_path <- 'H:/IGM project/Excel/output'
 
 
 #### CONFIGURATION ####
@@ -36,3 +43,21 @@ source(list.files(file.path("src", cfg$cohort_name), "03_", full.names = TRUE))
 source(list.files(file.path("src", cfg$cohort_name), "04_", full.names = TRUE))
 
 # the pre-processed cohort data file is now available in the scratch folder!
+
+
+
+#### SCRIPTS FOR ROBUSTNESS CHECKS ####
+
+# lifecycle bias for college attendance
+source(list.files(file.path("src/age21/robustness/"), "02", full.names = TRUE))
+
+source(list.files(file.path("src/age21/robustness/"), "03", full.names = TRUE))
+
+
+# lifecycle bias 
+source(list.files(file.path("src/age35/robustness/"), "02a", full.names = TRUE))
+
+source(list.files(file.path("src/age35/robustness/"), "03a", full.names = TRUE))
+
+source(list.files(file.path("src/age35/robustness/"), "03b", full.names = TRUE))
+
